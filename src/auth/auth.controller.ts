3f78cdce555d3537215dd06ab1053978cf78efde
saken78 <sakenspot@gmail.com>
2026-04-17 22:58:16 +0700

    feat: add register and login endpoints in auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,21 @@
 import { Hono } from "hono";
 import { authService } from "./auth.service";
-import { REGISTER_SCHEMA } from "./auth.model";
+import { LOGIN_SCHEMA, REGISTER_SCHEMA } from "./auth.model";
+import { HttpStatus } from "../utils/status_code";
 
 export const authController = new Hono();
 authController.post("/auth/register", async (c) => {
   const body = await c.req.json();
-  console.log(body);
   const validate = REGISTER_SCHEMA.parse(body);
-  const result = authService.register(validate);
-  return c.json(result, 201);
+  const result = await authService.register(validate);
+  return c.json(result, HttpStatus.CREATED);
+});
+authController.post("/auth/login", async (c) => {
+  const body = await c.req.json();
+  console.log(body);
+  const validate = LOGIN_SCHEMA.parse(body);
+  console.log(validate);
+  const result = await authService.login(validate);
+  console.log(result);
+  return c.json(result, HttpStatus.OK);
 });
