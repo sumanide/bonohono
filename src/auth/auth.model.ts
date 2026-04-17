3a6aaf12b3813879497634bdcaef69d7c9dc785e
saken78 <sakenspot@gmail.com>
2026-04-17 22:58:20 +0700

    feat: add auth model types for request/response

--- src/auth/auth.model.ts
+++ src/auth/auth.model.ts
@@ -7,4 +7,20 @@ export const REGISTER_SCHEMA = z.object({
   lastname: z.string().min(4).max(100).optional(),
 });
 
+export const LOGIN_SCHEMA = z.object({
+  email: z.string().email().min(1).max(100),
+  password: z.string().min(8).max(100),
+});
+
+export class UserResponse {
+  email!: string;
+  firstname?: string;
+}
+
+export class UserResponseQuery {
+  email!: string;
+  first_name!: string;
+}
+
 export type REGISTER_USER_REQUEST = z.infer<typeof REGISTER_SCHEMA>;
+export type LOGIN_USER_REQUEST = z.infer<typeof LOGIN_SCHEMA>;
