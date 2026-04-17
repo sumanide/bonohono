5602432c3c5657748933446c71f097897e4fd628
saken78 <sakenspot@gmail.com>
2026-04-17 22:58:23 +0700

    feat: implement auth service with register/login logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,25 +1,58 @@
 import { prismaService } from "../db/MariaDB";
-import { type REGISTER_USER_REQUEST } from "./auth.model";
+import bcrypt from "bcrypt";
+import {
+  type LOGIN_USER_REQUEST,
+  type REGISTER_USER_REQUEST,
+  UserResponse,
+  UserResponseQuery,
+} from "./auth.model";
+import { HttpStatus } from "../utils/status_code";
+import { HTTPException } from "hono/http-exception";
 
 export const authService = {
-  async register(req: REGISTER_USER_REQUEST) {
-    const result = await prismaService.$executeRaw`
-insert into
-    users (
+  async register(req: REGISTER_USER_REQUEST): Promise<UserResponse> {
+    const password = await bcrypt.hash(req.password, 12);
+    await prismaService.$executeRaw`
+    insert into users (
         email,
         password,
-        firstName,
-        lastName
-    )
-VALUES (
+        first_name,
+        last_name
+    ) VALUES (
         ${req.email},
-        ${req.password},
+        ${password},
         ${req.firstname},
         ${req.lastname}
     )`;
 
+    const user = await prismaService.users.findUnique({
+      where: { email: req.email },
+    });
+
+    if (user === undefined || !user) {
+      throw new HTTPException(HttpStatus.BAD_REQUEST, {
+        message: "User not found",
+      });
+    }
+
     return {
-      result,
+      email: user.email,
+    };
+  },
+  async login(req: LOGIN_USER_REQUEST): Promise<UserResponse> {
+    const result = await prismaService.users.findUnique({
+      where: { email: req.email },
+    });
+
+    if (result === undefined || !result) {
+      throw new HTTPException(HttpStatus.UNAUTHORIZED, {
+        message: "Unauthorized",
+      });
+    }
+
+    return {
+      email: result.email,
+      firstname: result.first_name,
     };
   },
 };
