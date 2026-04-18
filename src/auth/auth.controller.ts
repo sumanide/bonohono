ed083948e1f8cb5410a9a44b1e2be3e58036f6ab
saken78 <sakenspot@gmail.com>
2026-04-18 23:50:48 +0700

    feat: add logout and reset password endpoints, restructure response format

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,19 +1,44 @@
-import { Hono } from "hono";
+import { Hono, type Context } from "hono";
 import { authService } from "./auth.service";
-import { LOGIN_SCHEMA, REGISTER_SCHEMA } from "./auth.model";
+import {
+  LOGIN_SCHEMA,
+  REGISTER_SCHEMA,
+  RESET_PASSWORD_SCHEMA,
+} from "./auth.model";
 import { HttpStatus } from "../utils/status_code";
 
 export const authController = new Hono();
-authController.post("/auth/register", async (c) => {
+authController.post("/register", async (c: Context) => {
   const body = await c.req.json();
   const validate = REGISTER_SCHEMA.parse(body);
   const result = await authService.register(validate);
-  return c.json(result, HttpStatus.CREATED);
+  return c.json({
+    data: result,
+    status_code: HttpStatus.CREATED,
+  });
 });
-authController.post("/auth/login", async (c) => {
+authController.post("/login", async (c: Context) => {
   const body = await c.req.json();
   const validate = LOGIN_SCHEMA.parse(body);
-  const result = await authService.login(validate);
-  return c.json(result, HttpStatus.OK);
+  const result = await authService.login(validate, c);
+  return c.json({
+    data: result,
+    status_code: HttpStatus.OK,
+  });
 });
-authController.patch();
+authController.get("/logout", async (c: Context) => {
+  await authService.logout(c);
+  return c.json({
+    message: "Cookies cleared succesfully",
+    status_code: HttpStatus.OK,
+  });
+});
+authController.patch("/current", async (c: Context) => {
+  const body = await c.req.json();
+  const validate = RESET_PASSWORD_SCHEMA.parse(body);
+  await authService.reset_password(validate, c);
+  return c.json({
+    message: "Pasword changed succesfully",
+    status_code: HttpStatus.OK,
+  });
+});
