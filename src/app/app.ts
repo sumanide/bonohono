e6ceba6b6308dbcde82fe5e42b7636dd71d6fd93
saken78 <sakenspot@gmail.com>
2026-04-18 23:50:23 +0700

    feat: add global error handling middleware for HTTP and Zod errors

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,15 +4,35 @@ import { Hono } from "hono";
 import { prettyJSON } from "hono/pretty-json";
 import { logger } from "hono/logger";
 import figlet from "figlet";
+import { HTTPException } from "hono/http-exception";
+import { ZodError } from "zod";
 
 export const app = new Hono();
-app.use(prettyJSON({ force: true }));
-
-app.use(logger());
+app.use("/*", prettyJSON({ force: true }));
+app.use("/*", logger());
 app
   .basePath("/api")
   .route("/users", userController)
   .route("/auth", authController);
+
+app.onError(async (err, c) => {
+  if (err instanceof HTTPException) {
+    c.status(err.status);
+    return c.json({
+      errors: err.message,
+    });
+  } else if (err instanceof ZodError) {
+    c.status(400);
+    return c.json({
+      errors: err.issues,
+    });
+  } else {
+    c.status(500);
+    return c.json({
+      errors: err.message,
+    });
+  }
+});
 
 const body = figlet.textSync("saken");
 console.log(body);
