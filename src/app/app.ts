da82fa63df12e77a513b7354784bc62c84d03ad4
saken78 <sakenspot@gmail.com>
2026-04-18 21:35:26 +0700

    feat: refactor app.ts - migrate from js to ts structure

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,2 +1,22 @@
-export { userController } from "../user/user.controller";
-export { authController } from "../auth/auth.controller";
+import { userController } from "../user/user.controller";
+import { authController } from "../auth/auth.controller";
+import { Hono } from "hono";
+import { prettyJSON } from "hono/pretty-json";
+import { logger } from "hono/logger";
+import figlet from "figlet";
+
+export const app = new Hono();
+app.use(prettyJSON({ force: true }));
+
+app.use(logger());
+app
+  .basePath("/api")
+  .route("/users", userController)
+  .route("/auth", authController);
+
+const body = figlet.textSync("saken");
+console.log(body);
+
+for (let i = 0; i < app.routes.length; i++) {
+  console.log(`routes: ${app.routes[i]?.path}`);
+}
