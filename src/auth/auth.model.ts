e60a34a86a5b14e4f6eccaf75d2654d3d42e79ab
saken78 <sakenspot@gmail.com>
2026-04-18 23:50:38 +0700

    feat: add reset password schema and convert classes to type aliases

--- src/auth/auth.model.ts
+++ src/auth/auth.model.ts
@@ -3,8 +3,8 @@ import * as z from "zod";
 export const REGISTER_SCHEMA = z.object({
   email: z.string().email().min(1).max(100),
   password: z.string().min(8).max(100),
-  firstname: z.string().min(4).max(100),
-  lastname: z.string().min(4).max(100).optional(),
+  first_name: z.string().min(4).max(100),
+  last_name: z.string().min(4).max(100).optional(),
 });
 
 export const LOGIN_SCHEMA = z.object({
@@ -12,18 +12,23 @@ export const LOGIN_SCHEMA = z.object({
   password: z.string().min(8).max(100),
 });
 
-export class UserResponse {
-  email!: string;
-  firstname?: string;
-}
+export const RESET_PASSWORD_SCHEMA = z.object({
+  password: z.string().min(8).max(100),
+});
 
-export class UserResponseQuery {
-  email!: string;
-  first_name!: string;
-}
+export type UserResponse = {
+  email: string;
+  firstname?: string;
+};
+
+export type UserResponseQuery = {
+  email: string;
+  first_name: string;
+};
 
 export type REGISTER_USER_REQUEST = z.infer<typeof REGISTER_SCHEMA>;
 export type LOGIN_USER_REQUEST = z.infer<typeof LOGIN_SCHEMA>;
+export type RESET_PASSWORD_REQUEST = z.infer<typeof RESET_PASSWORD_SCHEMA>;
 
 export type JWT_PAYLOAD = {
   sub: string;
