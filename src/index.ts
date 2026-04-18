675cb9885455ea61da1a92b38aa6432ea7072749
saken78 <sakenspot@gmail.com>
2026-04-18 21:35:23 +0700

    feat: update entry point with app initialization

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,6 @@
-import { Hono } from "hono";
-import figlet from "figlet";
-import { userController, authController } from "./app/app.ts";
-import { prettyJSON } from "hono/pretty-json";
+import { app } from "./app/app";
 
-const app = new Hono();
-app.use(prettyJSON());
-const body = figlet.textSync("saken");
-
-app.route("/api", userController);
-app.route("/api", authController);
-
-console.log(`${body}`);
-for (let i = 0; i < app.routes.length; i++) {
-  console.log(`Routes: ${JSON.stringify(app.routes[i])}`);
-}
-
-export default {
-  port: 9999,
+Bun.serve({
+  port: process.env.PORT,
   fetch: app.fetch,
-};
+});
