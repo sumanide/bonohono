08606f0908997216c52d872463406935be9f9ef2
saken78 <sakenspot@gmail.com>
2026-04-18 23:50:50 +0700

    feat: clean up user routes and fix endpoint paths

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,12 +2,11 @@ import { Hono } from "hono";
 import { userService } from "./user.service.ts";
 
 export const userController = new Hono();
-userController.get();
-userController.get("/users", async (c) => {
+userController.get("/", async (c) => {
   const user = await userService.getAllUser();
   return c.json(user, 200);
 });
-userController.get("/users/:id", async (c) => {
+userController.get("/:id", async (c) => {
   const id = c.req.param("id");
   const user = await userService.getUserById(id);
   return c.json(user, 200);
