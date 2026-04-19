a4db895fef273d818c2985c24386717a29e4c799
saken78 <sakenspot@gmail.com>
2026-04-19 13:57:19 +0700

    feat: add AuthMiddleware for protected routes

---
+++ src/middleware/auth.middleware.ts
@@ -0,0 +1,33 @@
+import { type MiddlewareHandler } from "hono";
+import { verify } from "hono/jwt";
+import { SECRET } from "../utils/secret";
+import { getSignedCookie } from "hono/cookie";
+import { HTTPException } from "hono/http-exception";
+import { HttpStatus } from "../utils/status_code";
+import type { JWT_PAYLOAD } from "../auth/auth.model";
+
+export const AuthMiddleware: MiddlewareHandler = async (c, next) => {
+  if (!SECRET || SECRET === undefined) {
+    throw new HTTPException(HttpStatus.UNAUTHORIZED, {
+      message: "SECRET NOT FOUND",
+    });
+  }
+  const token = await getSignedCookie(c, SECRET, "refresh_token");
+  if (!token) {
+    throw new HTTPException(HttpStatus.UNAUTHORIZED, {
+      message: "UNAUTHORIZED",
+    });
+  }
+  let user: JWT_PAYLOAD;
+  try {
+    user = await verify(token, SECRET, "HS256");
+  } catch (err) {
+    throw new HTTPException(HttpStatus.UNAUTHORIZED, {
+      message: "Invalid or expired token",
+    });
+  }
+  c.set("user", {
+    email: user.email,
+  });
+  await next();
+};
