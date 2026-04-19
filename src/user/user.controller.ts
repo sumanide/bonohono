cc4ed84658c07b52e5e69c9aac49be3b27662cce
saken78 <sakenspot@gmail.com>
2026-04-19 13:56:53 +0700

    refactor: update user controller with proper typing and status codes

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,14 @@
-import { Hono } from "hono";
+import { Hono, type Context } from "hono";
 import { userService } from "./user.service.ts";
+import { HttpStatus } from "../utils/status_code.ts";
 
 export const userController = new Hono();
-userController.get("/", async (c) => {
+userController.get("/", async (c: Context) => {
   const user = await userService.getAllUser();
-  return c.json(user, 200);
+  return c.json(user, HttpStatus.OK);
 });
-userController.get("/:id", async (c) => {
+userController.get("/:id", async (c: Context) => {
   const id = c.req.param("id");
   const user = await userService.getUserById(id);
-  return c.json(user, 200);
+  return c.json(user, HttpStatus.OK);
 });
