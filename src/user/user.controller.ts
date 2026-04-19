f8c2cec4efaf6b18ddd53252d3c8df00d023255c
saken78 <sakenspot@gmail.com>
2026-04-19 21:49:02 +0700

    feat: protect user routes with AuthMiddleware

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,22 @@
 import { Hono, type Context } from "hono";
 import { userService } from "./user.service.ts";
 import { HttpStatus } from "../utils/status_code.ts";
+import { AuthMiddleware } from "../middleware/auth.middleware.ts";
 
 export const userController = new Hono();
-userController.get("/", async (c: Context) => {
+userController.use(AuthMiddleware);
+userController.get(async (c: Context) => {
   const user = await userService.getAllUser();
-  return c.json(user, HttpStatus.OK);
+  return c.json({
+    data: user,
+    status_code: HttpStatus.OK,
+  });
 });
 userController.get("/:id", async (c: Context) => {
-  const id = c.req.param("id");
+  const id = c.req.param("id") as string;
   const user = await userService.getUserById(id);
-  return c.json(user, HttpStatus.OK);
+  return c.json({
+    data: user,
+    status_code: HttpStatus.OK,
+  });
 });
