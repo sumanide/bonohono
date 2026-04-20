227765effa0c455b95be525656cc57fc5552094b
saken78 <sakenspot@gmail.com>
2026-04-20 23:19:12 +0700

    feat: add job controller with auth middleware

---
+++ src/job/job.controller.ts
@@ -0,0 +1,17 @@
+import { Hono, type Context } from "hono";
+import { JobService } from "./job.service";
+import { HttpStatus } from "../utils/status_code";
+import { winstonlogger } from "../utils/winston-logger";
+import { AuthMiddleware } from "../middleware/auth.middleware";
+
+export const JobController = new Hono();
+JobController.use(AuthMiddleware);
+JobController.post("/", async (c: Context) => {
+  const body = await c.req.json();
+  winstonlogger.info(body);
+  const result = await JobService.PostJob(body, c);
+  return c.json({
+    data: result,
+    status_code: HttpStatus.CREATED,
+  });
+});
