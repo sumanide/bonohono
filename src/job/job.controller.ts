6734e168c55f7adfdb5b5bb178733f970d387d5b
saken78 <sakenspot@gmail.com>
2026-04-21 18:03:30 +0700

    feat: add Job controller endpoints

--- src/job/job.controller.ts
+++ src/job/job.controller.ts
@@ -1,14 +1,12 @@
 import { Hono, type Context } from "hono";
 import { JobService } from "./job.service";
 import { HttpStatus } from "../utils/status_code";
-import { winstonlogger } from "../utils/winston-logger";
 import { AuthMiddleware } from "../middleware/auth.middleware";
 
 export const JobController = new Hono();
 JobController.use(AuthMiddleware);
 JobController.post("/", async (c: Context) => {
   const body = await c.req.json();
-  winstonlogger.info(body);
   const result = await JobService.PostJob(body, c);
   return c.json({
     data: result,
@@ -17,6 +15,14 @@ JobController.post("/", async (c: Context) => {
 });
 JobController.get("/", async (c: Context) => {
   const result = await JobService.GetAllJob();
+  return c.json({
+    data: result,
+    status_code: HttpStatus.OK,
+  });
+});
+JobController.get("/:id", async (c: Context) => {
+  const body = await c.req.json();
+  const result = await JobService.GetJobById(body);
   return c.json({
     data: result,
     status_code: HttpStatus.OK,
