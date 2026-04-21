7d4654a68fe9b759b1b71a39241d9c857755980f
saken78 <sakenspot@gmail.com>
2026-04-21 22:46:09 +0700

    feat: enhance Job service with additional business logic

--- src/job/job.service.ts
+++ src/job/job.service.ts
@@ -3,21 +3,45 @@ import { prismaService } from "../db/MariaDB";
 import {
   REGISTER_JOB_SCHEMA,
   type REGISTER_JOB,
-  type RegisterJobResult,
-  type GetAllJobResult,
-  type GetJobById,
+  type GetJobResult,
 } from "./job.model";
 import { winstonlogger } from "../utils/winston-logger";
+import { HTTPException } from "hono/http-exception";
+import { HttpStatus } from "../utils/status_code";
+
 export const JobService = {
-  async GetAllJob(): Promise<GetAllJobResult[]> {
+  async GetAllJob(): Promise<GetJobResult[]> {
     const jobs = await prismaService.jobs.findMany();
     return jobs;
   },
-  async GetJobById(req: GetJobById): Promise<GetAllJobResult> {
-    const jobs = await prismaService.jobs.findUnique({ where: { id: req.id } });
-    return jobs;
+  async GetJobById(id: string): Promise<GetJobResult> {
+    winstonlogger.debug("executed: ");
+    const jobs = await prismaService.jobs.findUnique({ where: { id: id } });
+    if (!jobs) {
+      throw new HTTPException(HttpStatus.BAD_REQUEST, {
+        message: "Job not found",
+      });
+    }
+    return {
+      id: jobs.id,
+      poster_id: jobs.poster_id,
+      title: jobs.title,
+      description: jobs.description,
+      category_id: jobs.category_id,
+      budget: jobs.budget,
+      status: jobs.status,
+      deadline: jobs.deadline,
+      location: jobs.location,
+      work_type: jobs.work_type,
+      commitment: jobs.commitment,
+      experience_level: jobs.experience_level,
+      payment_type: jobs.payment_type,
+      skills: jobs.skills,
+      created_at: jobs.created_at,
+      updated_at: jobs.updated_at,
+    };
   },
-  async PostJob(req: REGISTER_JOB, c: Context): Promise<RegisterJobResult> {
+  async PostJob(req: REGISTER_JOB, c: Context): Promise<GetJobResult> {
     const request = REGISTER_JOB_SCHEMA.parse(req);
     const user_id = c.get("user");
     winstonlogger.debug(user_id.sub);
@@ -34,10 +58,24 @@ export const JobService = {
         work_type: request.work_type ?? null,
         commitment: request.commitment,
       },
-      select: { title: true },
     });
     return {
+      id: job.id,
+      poster_id: job.poster_id,
       title: job.title,
+      description: job.description,
+      category_id: job.category_id,
+      budget: job.budget,
+      status: job.status,
+      deadline: job.deadline,
+      location: job.location,
+      work_type: job.work_type,
+      commitment: job.commitment,
+      experience_level: job.experience_level,
+      payment_type: job.payment_type,
+      skills: job.skills,
+      created_at: job.created_at,
+      updated_at: job.updated_at,
     };
   },
 };
